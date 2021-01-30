ffe994a59da9044419574b8e44dff6db59b374a9
kumlowhup <kumlowhup@yahoo.co.jp>
2021-01-30 16:22:38 +0800

    completed Hotel

--- src/Hotel.java
+++ src/Hotel.java
@@ -2,17 +2,20 @@ public class Hotel {
     private String name;
     private Room[][] rooms;
 
-
     public Room[][] getRooms() {
         return rooms;
     }
 
-    public void setRooms(Room[][] rooms) {
-        this.rooms = rooms;
+    public void booking(int i) {
+        rooms[i / 100 - 1][i % 100 - 1].booking();
+    }
+
+    public void exiting(int i) {
+        rooms[i / 100 - 1][i % 100 - 1].exiting();
     }
 
     public Hotel() {
-        Room[][] rooms = new Room[5][10];
+        rooms = new Room[5][10];
         for (int i = 0 ;i < rooms.length ;i++){
             for(int j = 0;j < rooms[i].length ;j++){
                 rooms[i][j] = new Room(i,j);
--- src/MyHotel.java
+++ src/MyHotel.java
@@ -1,17 +1,45 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class MyHotel {
 
     public static void main(String[] args) {
 
         Hotel hotel = new Hotel();
-        printRooms(hotel.getRooms());
+        //System.out.println(hotel);
+        Scanner s = new Scanner(System.in);
+        System.out.println("欢迎使用酒店系统");
+        while(true){
+            System.out.println("酒店管理如下： ");
+            printRooms(hotel.getRooms());
+            System.out.print("请输入管理业务序号：");
+            int i  = s.nextInt();
+            switch (i){
+                //订房
+                case 0 :{
+                    System.out.println("请输入订房的房间号码：");
+                    hotel.booking(s.nextInt());
+                } break;
 
+                //退房
+                case 1:{
+                    System.out.println("请输入退房的房间号码：");
+                    hotel.exiting(s.nextInt());
+                } break;
+                case 2:{
+                    System.out.println("酒店系统已退出！");
+                    return;
+                }
+                default:
+                    throw new IllegalStateException("输入错误，数字: " + i);
+            }
+
+        }
 
     }
 
 
-    public static void printRooms(Object hotel[][]){
+    public static void printRooms(Object[][] hotel){
         for (int i = 0 ;i < hotel.length ;i++){
             for(int j = 0;j < hotel[i].length ;j++){
                 System.out.print(hotel[i][j]);
@@ -34,11 +62,11 @@ class Room{
     public Room(int i,int j) {
         this.number = ( i + 1 ) * 100 + j + 1;
         switch (i){
-            case 0 :attribute = "钟点房";break;
+            case 0 :attribute = "钟点房   ";break;
             case 1 :attribute = "普通单人房";break;
             case 2 :attribute = "普通双人房";break;
-            case 3 :attribute = "情侣套房";break;
-            case 4 :attribute = "总统套房";break;
+            case 3 :attribute = "情侣套房  ";break;
+            case 4 :attribute = "总统套房  ";break;
             default:
                 throw new IllegalStateException("房间信息错误！\nUnexpected value: " + i);
         }
@@ -48,7 +76,7 @@ class Room{
     @Override
     public String toString() {
 
-        return  attribute + number + " " + ( status ? "客 " : "空 " );
+        return  '['+attribute + number + " " + ( status ? "空]" : "客]" );
 
     }
 
@@ -66,8 +94,13 @@ class Room{
     }
 
     //改变房间空房/有客的状态
-    public void setFree() {
-        status = !status;
-        System.out.println("房间" + number + "已经" + (status ? "订" : "退" ) + "房");
+    public void booking() {
+        status = false;
+        System.out.println("房间" + this + "已经订房");
+    }
+
+    public void exiting(){
+        status = true;
+        System.out.println("房间" + this + "已经退房");
     }
 }
